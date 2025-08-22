9d0b792c87e5625ffdb447ca9ac70c06e25f5a72
Alro127 <nhuttan373@gmail.com>
2025-08-22 10:49:39 +0700

    get all department

---
+++ src/main/java/com/example/ticket_helpdesk_backend/controller/DepartmentController.java
@@ -0,0 +1,35 @@
+package com.example.ticket_helpdesk_backend.controller;
+
+import com.example.ticket_helpdesk_backend.dto.ApiResponse;
+import com.example.ticket_helpdesk_backend.dto.DepartmentBasicInfoDto;
+import com.example.ticket_helpdesk_backend.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/department")
+@RequiredArgsConstructor
+public class DepartmentController {
+    @Autowired
+    private final DepartmentService departmentService;
+
+    @GetMapping("/get-all")
+    public ResponseEntity<?> getAllDepartments() {
+        List<DepartmentBasicInfoDto> departmentBasicInfoDtoList = departmentService.getDepartmentBasicInfoDtoList();
+        ApiResponse<List<DepartmentBasicInfoDto>> apiResponse = new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "All departments found",
+                LocalDateTime.now(),
+                departmentBasicInfoDtoList);
+
+        return ResponseEntity.ok(apiResponse);
+    }
+}
